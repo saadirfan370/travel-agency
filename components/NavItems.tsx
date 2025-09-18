c2c5d51e1f91ac11c8e1171241bd787c8e9ca9df
Saad Irfan <saadirfan370@gmail.com>
2025-09-18 21:21:23 +0500

    Add MobileSidebar component and integrate it into AdminLayout; update NavItems to handle sidebar toggle

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -3,7 +3,13 @@ import { Link, NavLink } from "react-router";
 import { sidebarItems } from "~/constants";
 import { cn } from "~/lib/utils";
 
-const NavItems = () => {
+const NavItems = ({ handleClick }: { handleClick?: () => void }) => {
+  const user = {
+    name: "saad",
+    email: "saad@gmail.com",
+    imageUrl: "/assets/images/david.webp",
+  };
+
   return (
     <section className="nav-items">
       <Link to="/" className="link-logo">
@@ -20,13 +26,44 @@ const NavItems = () => {
                   className={cn("group nav-item", {
                     "bg-primary-100 text-white": isActive,
                   })}
+                  onClick={handleClick}
                 >
+                  <img
+                    src={icon}
+                    alt={label}
+                    className={`group-hover:brightness-0 size-0 group-hover:invert ${isActive ? "brightness-0 invert" : "text-dark-200"}`}
+                  />
                   {label}
                 </div>
               )}
             </NavLink>
           ))}
         </nav>
+
+        <footer className="nav-footer">
+          <img
+            src={user?.imageUrl || "/assets/images/david.webp"}
+            alt={user.name || "David"}
+          />
+
+          <article>
+            <h2>{user.name}</h2>
+            <p>{user?.email}</p>
+          </article>
+
+          <button
+            onClick={() => {
+              console.log("logout");
+            }}
+            className="cursor-pointer"
+          >
+            <img
+              src="/assets/icons/logout.svg"
+              alt="logout"
+              className="size-6"
+            />
+          </button>
+        </footer>
       </div>
     </section>
   );
